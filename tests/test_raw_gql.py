655caedfb1e76a66ffedad529dc32e35f44fc113
James Williams <jamwil@gmail.com>
2018-10-25 23:20:16 -0600

    Hello world!

---
+++ tests/test_raw_gql.py
@@ -0,0 +1,40 @@
+import pytest
+import raw_gql
+
+def test_string_query():
+    raw_gql.GQL_FOLDER = 'tests/gql/'
+    query = """
+        {
+            users {
+                id
+                name
+            }
+        }
+    """
+    db = raw_gql.GraphQL('https://us1.prisma.sh/james-williams-a19027/test-db/dev')
+    r = db.query(query)
+
+    assert type(r) is dict
+    assert type(r['data']['users']) is list
+
+
+def test_file_query():
+    raw_gql.GQL_FOLDER = 'tests/gql/'
+    query = 'q_users.gql'
+    db = raw_gql.GraphQL('https://us1.prisma.sh/james-williams-a19027/test-db/dev')
+    r = db.query(query)
+
+    assert type(r) is dict
+    assert type(r['data']['users']) is list
+
+
+def test_variables_query():
+    raw_gql.GQL_FOLDER = 'tests/gql/'
+    query = 'q_var_user.gql'
+    vars = {'name': 'James Williams'}
+    db = raw_gql.GraphQL('https://us1.prisma.sh/james-williams-a19027/test-db/dev')
+    r = db.query(query, vars)
+
+    assert type(r) is dict
+    assert type(r['data']['users']) is list
+    assert r['data']['users'][0]['name'] == 'James Williams'
